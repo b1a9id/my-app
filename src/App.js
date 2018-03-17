cd608ac2634a1fd469d254bd6838d1ea13d1c559
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-03-17 21:53:21 +0900

    tasksをstateで管理する

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
-  render() {
-  	const tasks = [
-  		{title: 'Todo1st', id: 0},
-	    {title: 'Todo2nd', id: 1}
-	    ];
-    return (
-        <div>
-	        <h1>TODO App</h1>
-	        <TodoInput />
-	        <TodoList tasks={tasks} />
-        </div>
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: [
+				{
+					title: 'デフォルトTODO',
+					id: 0,
+				}
+			],
+			uniqueId: 1,
+		};
+	}
+	render() {
+		return (
+			<div>
+				<h1>TODO App</h1>
+				<TodoInput/>
+				<TodoList tasks={this.state.tasks}/>
+			</div>
 
-    );
-  }
+		);
+	}
 }
 
 export default App;
